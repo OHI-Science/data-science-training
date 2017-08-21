f6a7833d0955a5473348fe0dc9b3ecb25846c5d5
jules32 <jstewart@nceas.ucsb.edu>
2017-08-21 16:53:16 -0700

    first pass outline

--- _deploy.r
+++ _deploy.r
@@ -7,21 +7,23 @@
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
 ## read in github token outside of repo, generated with url above 
-gh_token <- scan('~/.github-token', 'character', quiet = T)
+gh_token <- scan('~/.github-token', 'character', quiet = TRUE)
 Sys.setenv(GH_TOKEN=gh_token)
 
 ## book variables
-repo_name = 'toolbox-training'
-repo_slug = sprintf('OHI-Science/toolbox-training')
-repo_url = sprintf('https://github.com/%s', repo_slug)
-dir_gh_pages = sprintf('~/github/clip-n-ship/%s', repo_name)
-dir_master = sprintf('~/github/%s', repo_name)
-dir_master_book = sprintf('~/github/%s/_book', repo_name)
+repo_name <- 'data-science-training'
+repo_slug <- sprintf('OHI-Science/%s', repo_name)
+repo_url <- sprintf('https://github.com/%s', repo_slug)
+dir_gh_pages <- sprintf('~/github/clip-n-ship/%s', repo_name)
+dir_master <- sprintf('~/github/%s', repo_name)
+dir_master_book <- sprintf('~/github/%s/_book', repo_name)
 
 
 ## clone gh-pages branch to temporary location
-unlink(dir_gh_pages, recursive=T, force=T) # delete if temporary location exists
-repo = git2r::clone(url=repo_url, local_path=normalizePath(dir_gh_pages, mustWork=F), branch='gh-pages')
+unlink(dir_gh_pages, recursive=TRUE, force=TRUE) # delete if temporary location exists
+repo <- git2r::clone(url=repo_url, 
+                     local_path=normalizePath(dir_gh_pages, mustWork=FALSE), 
+                     branch='gh-pages')
 
 ## copy master branch _book/*.html files to gh-pages
 system(sprintf('cp %s/* %s', dir_master_book, dir_gh_pages))
@@ -30,7 +32,7 @@ system(sprintf('cp %s/* %s', dir_master_book, dir_gh_pages))
 
 ## add and commit gh-pages branch
 git2r::add(repo, sprintf('%s/*html', dir_gh_pages))
-git2r::commit(repo, message="updating bookdown from toolbox_training@master::_deploy.r")
+git2r::commit(repo, message=sprintf("updating bookdown from %s@master::_deploy.r", repo_name))
 
 setwd(dir_gh_pages)
 system(sprintf('git push https://%s@github.com/%s.git HEAD:%s', gh_token, repo_slug, 'gh-pages'))
